2e11b0746a0a37574cd3789b23fa181f701daf1b
Mandela Mitau <digitalcreative@mandelamitau.com>
2023-12-06 15:29:22 +0300

    update projects' urls

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,13 +2,13 @@ export const projects = [
   {
     title: 'Movie central',
     description: "A react project for searching and viewing movie image and release year from the OMDb API  a RESTful web service to obtain movie information",
-      image: '/images/moviecentral.png',
-      tags: [ 'React'],
-    live: 'https://moviecentral.projects.mandelamitau.com/',
+    image: '/images/moviecentral.png',
+    tags: ['React'],
+    live: 'https://moviecentral_projects.mandelamitau.com/',
     visit: 'https://github.com/main-tech/movie_central',
     id: 0,
   },
-  
+
 ];
 
 export const TimeLineData = [
