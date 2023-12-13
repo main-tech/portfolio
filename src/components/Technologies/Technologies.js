a9240551fd8e87f737b8589f34dfea06f0849785
Mandela M Muithi <digitalcreative@mandelamitau.com>
2023-12-13 22:59:47 +0300

    update technologies used

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,90 +1,151 @@
 import React from 'react';
-import { DiFirebase, DiJava, DiProlog, DiReact} from 'react-icons/di';
+import { DiFirebase, DiJava, DiProlog, DiReact } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 import { RiVuejsFill } from "react-icons/ri";
 import { SiCapacitor } from "react-icons/si";
-const Technologies = () =>  (
-<Section id="tech">
-  <SectionDivider/>
-  <SectionTitle>Technologies</SectionTitle>
+import { FaAws } from "react-icons/fa";
+import { DiDocker } from 'react-icons/di';
+import { DiNginx } from 'react-icons/di';
+import { SiTraefikproxy } from 'react-icons/si';
+import { SiMicrosoftazure } from 'react-icons/si';
+import { SiKubernetes } from 'react-icons/si';
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      Showcased below are the tools I use to build amazing projects
+      Core Tools & Technologies: My Go-To Resources for Project Excellence
     </SectionText>
     <List>
-    <ListItem>
+      {/* AWS */}
+      <ListContainer>
+        <FaAws size="3rem" />
+        <ListTitle>AWS</ListTitle>
+        <ListParagraph>
+          <p>Backbone of Cloud Innovation</p>
+        </ListParagraph>
+      </ListContainer>
+
+
+      {/* Azure */}
+      <ListContainer>
+        <SiMicrosoftazure size="3rem" />
+        <ListTitle>Azure</ListTitle>
+        <ListParagraph>
+          <p>Powering Scalable, Secure Cloud Solutions</p>
+        </ListParagraph>
+      </ListContainer>
+
+
+      {/* Docker */}
+      <ListContainer>
+        <DiDocker size="3rem" />
+        <ListTitle>Docker</ListTitle>
+        <ListParagraph>
+          <p>Streamlining Development with Containerization</p>
+        </ListParagraph>
+      </ListContainer>
+
+      <ListContainer>
+        <SiKubernetes size="3rem" />
+        <ListTitle>Kubernetes</ListTitle>
+        <ListParagraph>
+          <p>Orchestrating Container Efficiency and Reliability</p>
+        </ListParagraph>
+      </ListContainer>
+      {/* Nginx */}
+      <ListContainer>
+        <DiNginx size="3rem" />
+        <ListTitle>Nginx</ListTitle>
+        <ListParagraph>
+          <p>Accelerating Web Performance and Stability</p>
+        </ListParagraph>
+      </ListContainer>
+
+
+      {/* Traefik */}
+      <ListContainer>
+        <SiTraefikproxy size="3rem" />
+        <ListTitle>Traefik</ListTitle>
+        <ListParagraph>
+          <p>Simplifying Network Management with Intelligent Routing</p>
+        </ListParagraph>
+      </ListContainer>
+
+      <ListItem>
         <ListContainer>
-        <DiProlog size="3rem"/>
-      <ListTitle>Prolog</ListTitle>
-      <ListParagraph>
-        <p>Problem Solving</p>
-      </ListParagraph>
+          <DiProlog size="3rem" />
+          <ListTitle>Prolog</ListTitle>
+          <ListParagraph>
+            <p>Problem Solving</p>
+          </ListParagraph>
         </ListContainer>
       </ListItem>
 
       <ListItem>
         <ListContainer>
-        <DiJava size="3rem"/>
-      <ListTitle>Java</ListTitle>
-      <ListParagraph>
-      </ListParagraph>
-      Standard Edition
+          <DiJava size="3rem" />
+          <ListTitle>Java</ListTitle>
+          <ListParagraph>
+          </ListParagraph>
+          Standard Edition
         </ListContainer>
       </ListItem>
 
       <ListItem>
         <ListContainer>
-        <DiReact size="3rem"/>
-      <ListTitle>React</ListTitle>
-      <ListParagraph>
-      </ListParagraph>
-       <p>Client side</p>
+          <DiReact size="3rem" />
+          <ListTitle>React</ListTitle>
+          <ListParagraph>
+          </ListParagraph>
+          <p>Client side</p>
         </ListContainer>
       </ListItem>
-      
-
-      <ListItem>
-        <ListContainer>
-        <RiVuejsFill size="3rem"/>
-      <ListTitle>Vue</ListTitle>
-      <ListParagraph>
-      <p>Client side</p>
-      </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
 
 
       <ListItem>
         <ListContainer>
-        <SiCapacitor size="3rem"/>
-      <ListTitle>Capacitor</ListTitle>
-      <ListParagraph>
-        <p>Cross platform native mobile applications</p>
-      </ListParagraph>
+          <RiVuejsFill size="3rem" />
+          <ListTitle>Vue</ListTitle>
+          <ListParagraph>
+            <p>Client side</p>
+          </ListParagraph>
         </ListContainer>
       </ListItem>
-
-    
-
 
 
 
       <ListItem>
         <ListContainer>
-        <DiFirebase size="3rem"/>
-      <ListTitle>Back-End</ListTitle>
-      <ListParagraph>
-        Experience with <br/>
-        NOde.js and Data bases
-      </ListParagraph>
+          <SiCapacitor size="3rem" />
+          <ListTitle>Capacitor</ListTitle>
+          <ListParagraph>
+            <p>Cross platform native mobile applications</p>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+
+
+
+
+
+      <ListItem>
+        <ListContainer>
+          <DiFirebase size="3rem" />
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            NOde.js and Data bases
+          </ListParagraph>
         </ListContainer>
       </ListItem>
 
 
 
     </List>
-</Section>
+  </Section>
 );
 
 export default Technologies;
