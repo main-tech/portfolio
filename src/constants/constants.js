c7229b637a032e63de06fa67c4eb335d5ed7dbfc
Mandela Mitau <digitalcreative@mandelamitau.com>
2023-12-19 10:13:35 +0300

    update movie central project url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@ export const projects = [
     description: "A react project for searching and viewing movie image and release year from the OMDb API  a RESTful web service to obtain movie information",
     image: '/images/moviecentral.png',
     tags: ['React'],
-    live: 'https://moviecentral_projects.mandelamitau.com/',
+    live: 'https://moviecentral-projects.mandelamitau.com/',
     visit: 'https://github.com/main-tech/movie_central',
     id: 0,
   },
